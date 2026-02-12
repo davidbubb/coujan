4053b2015d32988fa75eba1f22e8bb0726d9b3ea
David Bubb <David.Bubb@cityandguilds.com>
2026-02-12 15:11:30 +0000

    fix: update app name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('vite');
 module.exports = async () => {
   const { svelte } = await import('@sveltejs/vite-plugin-svelte');
   return defineConfig({
-    base: '/caoren/',
+    base: '/Coujan/',
     plugins: [svelte()]
   });
 };
